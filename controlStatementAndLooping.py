2ae481463e1a7de7f4d67f5c4e52e2328af33c7e
rwa4shivamum <rwa4.shivam.um@gmail.com>
2026-07-07 18:55:50 +0530

    minorButImp

---
+++ controlStatementAndLooping.py
@@ -0,0 +1,149 @@
+# for i in range(3):
+#     print(i)
+# else:
+#     print("done")
+
+# for i in range(3):
+#     if i==2:
+#         print(i)
+#         break
+# else:
+#     print("Loop finished") why this else won't work
+
+# i = 0
+# while i<5:
+#     print(i)
+#     i+=1
+
+# for i in range(1,11):
+#     print(i)
+#     i+=2
+
+
+# for i in range(5,0,-1):
+#     print(i)
+
+# i=5
+# while(i>0):
+#     print(i)
+#     i -=1
+
+# for i in range(5,0,-2):
+#     print(i)
+
+# for i in "1231bc":
+#     print(i)
+
+# i = 1
+# while i = 10:
+#     i += 2
+# for i in range(1):
+#     print("Hi")
+
+# print(list(range(10,0,-1))) #prblm 13
+# for i in range(1, 10, 2):
+#     print(i)
+
+# for i in range(-5): #range(0, -5, 1)
+#     print(i)
+
+# for i in range(5, -5, -1):
+#     print(i, end=" ")
+
+# for i in range(-3, 3, 1):
+#     print(i, end=" ")
+
+# for i in range(0):
+#     print("Hello")
+# else:
+#     print("Done")
+# for i in range(3):
+#     for j in range(i):
+#         print(i, j)
+
+# for i in range(1, 11, 3):
+#     print(i, end=" ")
+
+# print(list(range(10, 10)))
+
+
+lst = []
+
+# for i in range(1,4):
+#     lst.append(i)
+
+# print(lst)
+# lst = []
+# lst5 = []
+# for i in range(0,100):
+#     if i%3==0:
+#         lst.append(i)
+#     elif i%5==0:
+#         lst5.append(i)
+
+# print(lst)
+# print(lst5)
+
+# print([x if x%2==0 else -x for x in range(5)])
+
+# print([(i,j) for i in range(2) for j in range(3)])
+
+# for i in range(3):
+#     print(i)
+#     i = 5
+
+# lst = [1,2,5,3]
+# for i in lst:
+#     print(i)
+
+# for i in range(5):
+#     if i == 2:
+#        continue
+#     print(i)
+# else:
+#     print("Done")
+
+# lst = [1,2,3,4,5]
+# sum = 0
+# for i in lst:
+#     sum += i
+
+# print(sum)
+# n = 121
+# num = n
+# reverse = 0
+# while num > 0:
+#     digit = num % 10
+#     reverse = reverse * 10 + digit
+#     num = num // 10
+
+# print(reverse == n)
+
+
+# str = "jahdjkhsbfdoqiuwey"
+# count = 0
+# for i in str:
+#     if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u"):
+#         count += 1
+    
+# print(count)
+
+lst = []
+
+def isPrime(n):
+    count = 0
+    for i in range(1,n+1):
+        if(n%i==0):
+            count += 1
+
+    if(count == 2):
+        return True
+    else:
+        return False
+
+print(isPrime(2))
+# for i in range(1,101):
+#     if(isPrime(i)):
+#         lst.append(i)
+
+print(lst)
--- inputPrint.py
+++ inputPrint.py
@@ -1,110 +1,201 @@
-# print("Hello", "world" , sep="-", end="!")
+# # print("Hello", "world" , sep="-", end="!")
 
-# name = "Shivam"
-# age = 20
+# # name = "Shivam"
+# # age = 20
 
-# print(f"My name is {name} and age is {age}")
-# #f-string based
+# # print(f"My name is {name} and age is {age}")
+# # #f-string based
 
-# #print("Hello" + 5)#Error
+# # #print("Hello" + 5)#Error
 
-# print("Hello" + str(4))
+# # print("Hello" + str(4))
 
-# num = input("Enter Number:")
-# print(int(num) + 5)
+# # num = input("Enter Number:")
+# # print(int(num) + 5)
 
-# # name = "2"
-# if name.isdigit():
-#     name = int(name)
-# else:
-#     print("Invalid Number")
-
-
-# x = 10
-# name = "shivam"
-
-# #vairable
-# x = 10
-# name = "shivam"
-
-#dataTypes int, float, str, bool, list, tuple, dict
-
-print(type(2.5))#float
-print(type("2"))#string
-print(type(2))#int
-print(type(True))#bool
-print(type([1,2,3]))#list
-print(type((1,2,3)))#tuple
-print(type({"a":1}))#dictionary
+# # # name = "2"
+# # if name.isdigit():
+# #     name = int(name)
+# # else:
+# #     print("Invalid Number")
 
 
-#Arithmetic operator
-#+,-,*,/,//,%,**
-print(2+4)
-print(2-2)
-print(2*2)
-print(2/2)
-print(10//3)
-print(10%2)
-print(10**2)
+# # x = 10
+# # name = "shivam"
 
-#comparision
-print(10>1, 20<3, 20!=20, 10==2, 10>=2)
+# # #vairable
+# # x = 10
+# # name = "shivam"
 
-#logical operator
-print(10>1 and 10>5)
-print(10>2 or 20 < 15)
-print(not 10>2)#always toggle the bool
-print(-5 // 2)  # -3  ❗ (important))
+# #dataTypes int, float, str, bool, list, tuple, dict
 
-#type Casting 
-print(type(int("10")))
-print(type(float("10.5")))
-print(type(str(10)))
-
-# int("10.5") erorr 
-print(type(int(float("10.5"))))
+# print(type(2.5))#float
+# print(type("2"))#string
+# print(type(2))#int
+# print(type(True))#bool
+# print(type([1,2,3]))#list
+# print(type((1,2,3)))#tuple
+# print(type({"a":1}))#dictionary
 
 
-#implicit type casting
-a = 5
-b = 2.5
+# #Arithmetic operator
+# #+,-,*,/,//,%,**
+# print(2+4)
+# print(2-2)
+# print(2*2)
+# print(2/2)
+# print(10//3)
+# print(10%2)
+# print(10**2)
 
-c = a+b
-print(c)
-print(type(c)) #float converted by python itself
+# #comparision
+# print(10>1, 20<3, 20!=20, 10==2, 10>=2)
 
-#what is complex in pthon ?
-# real part + imaginary part in python
-# a + bj
-# a=real part b = imaginary part and j= √(-1)
+# #logical operator
+# print(10>1 and 10>5)
+# print(10>2 or 20 < 15)
+# print(not 10>2)#always toggle the bool
+# print(-5 // 2)  # -3  ❗ (important))
 
-x = 3 + 2j
-print(x.real, type(x.real))#3.0 always be in flaot
-print(x.imag, type(x.imag))#2.0
-print(type(x))#complex
+# #type Casting 
+# print(type(int("10")))
+# print(type(float("10.5")))
+# print(type(str(10)))
 
-print((2+3j) + (3+3j),"addition")
-print((5 + 3j) - (2 + 1j), "subtraction")
-print((1 + 2j) * (3 + 4j), "multiplication")
-print((1 + 2j) / (1 + 1j), "divide")
+# # int("10.5") erorr 
+# print(type(int(float("10.5"))))
 
-#🧠 Summary
-# Complex = a + bj
-# j = imaginary unit
-# Supports math operations
-# No comparison allowed
-# Use .real & .imag
-# Created using complex()
-x = 3 + 4j
 
-print(x.real)
-print(x.imag)
-print(abs(x))
-print(x.conjugate())
-print(type(x))
+# #implicit type casting
+# a = 5
+# b = 2.5
 
-#int->float->complex
+# c = a+b
+# print(c)
+# print(type(c)) #float converted by python itself
 
-print(float(2))
-print(  )
+# #what is complex in pthon ?
+# # real part + imaginary part in python
+# # a + bj
+# # a=real part b = imaginary part and j= √(-1)
+
+# x = 3 + 2j
+# print(x.real, type(x.real))#3.0 always be in flaot
+# print(x.imag, type(x.imag))#2.0
+# print(type(x))#complex
+
+# print((2+3j) + (3+3j),"addition")
+# print((5 + 3j) - (2 + 1j), "subtraction")
+# print((1 + 2j) * (3 + 4j), "multiplication")
+# print((1 + 2j) / (1 + 1j), "divide")
+
+# #🧠 Summary
+# # Complex = a + bj
+# # j = imaginary unit
+# # Supports math operations
+# # No comparison allowed
+# # Use .real & .imag
+# # Created using complex()
+# x = 3 + 4j
+
+# print(x.real)
+# print(x.imag)
+# print(abs(x))
+# print(x.conjugate())
+# print(type(x))
+
+# #int->float->complex
+
+# print(float(2))
+
+# #bool conversion
+# #True = 1 and false = 0
+# print(int(True))
+# print(int(False))
+
+# print(float(True))
+# print(float(False))
+
+# print(bool(0))
+# print(bool(1))
+# print(bool(-1))
+# print(bool(""))
+# print(bool("Hello"))
+# print(bool([]))
+
+# bool([1,2])
+
+# print(int(float("10.5")))
+
+# | From \ To | int | float | bool | str |
+# | --------- | --- | ----- | ---- | --- |
+# | int       | ✔️  | ✔️    | ✔️   | ✔️  |
+# | float     | ✔️  | ✔️    | ✔️   | ✔️  |
+# | bool      | ✔️  | ✔️    | ✔️   | ✔️  |
+# | str       | ⚠️  | ⚠️    | ✔️   | ✔️  |
+# ⚠️ = Only if valid format
+
+x = 10
+#print(id(x))
+
+# # a = 10
+# # b = 10
+
+# a = 100000000000
+# b = 100000000000
+
+# print(id(a), id(b))
+
+# x = [1,2]
+# y = x
+
+# y.append(3)
+
+# print(x)  # [1,2,3]
+# print(10 + int("5"))   # Try breaking
+
+# print("10","20", "30", sep="-")
+
+# name = input("Enter your name:")
+# print("Hello", name)
+# a=float(input("Enter num1:"))
+# b=int(input("Enter num2:"))
+
+# print(a*b)
+
+# a=bool(input("Enter numbe:"))
+# print(type(a),a)
+
+# print(type("10"))
+# print(type(10))
+# print(type(10.0))
+# print(int("123"))
+# print(str(50))
+# print(int(10.9))
+# print(int("abc"))#error write a valid number
+# print(float("10a"))#ValueError: could not convert string to float: '10a
+
+# print(int(float("10.5")))
+
+# print(list("hello"))#['h', 'e', 'l', 'l', 'o']
+# print(set("hello"))#{'o', 'e', 'l', 'h'}
+
+# print(int("1000"))
+# print(float("100.5"))
+# print(str(100))
+# print(tuple([1,2,3]))
+# print(list((1,2,3)))
+
+a = 10
+b = 10
+# print(id(a), id(b))
+
+# ab = 100000000000000000000000000000000000000033333222223222333222222222222
+# bv = 100000000000000000000000000000000000000033333222223222333222222222222
+
+# print(id(ab), id(bv))
+
+a = "10"
+b = 5
+print("%" * 3)#here mutiple of this modulo
+print(a + str(b) * 3)
